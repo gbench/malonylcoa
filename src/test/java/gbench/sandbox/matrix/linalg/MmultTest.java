8cf10d64650efcd2ec7a184612665b09514e506a
gbench <gbench@sina.com>
2023-07-20 14:11:05 +0800

    增加测试

--- src/test/java/gbench/sandbox/matrix/linalg/MmultTest.java
+++ src/test/java/gbench/sandbox/matrix/linalg/MmultTest.java
@@ -10,17 +10,21 @@ public class MmultTest {
 
 	@Test
 	public void foo() {
-		final var n = 400;
-		final var nx = INdarray.nd(i -> i * 1d, n * n).nx(n);
-		println(nx.mmult(nx));
-
+		for (int k = 100; k <= 1000; k += 100) {
+			long begtime = System.currentTimeMillis();
+			final var n = k;
+			final var nx = INdarray.nd(i -> i * 1d, n * n).nx(n);
+			double d = nx.mmult(nx).get();
+			long endtime = System.currentTimeMillis();
+			println(k, "lasts", endtime - begtime, "last result row", d);
+		}
 	}
-	
+
 	@Test
 	public void bar() {
 		final var m = 5;
 		final var n = 2;
-		final var nx = INdarray.nd(i -> i * 1d, m*n).nx(n);
+		final var nx = INdarray.nd(i -> i * 1d, m * n).nx(n);
 		println(nx.mmult(nx.transpose()));
 
 	}
