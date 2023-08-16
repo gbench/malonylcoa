e9f8e364b85a4d4a33a3b7fc8497da061953e961
gbench <gbench@sina.com>
2023-08-16 14:05:10 +0800

    增加函数

--- src/test/java/gbench/sandbox/data/h2/H2db.java
+++ src/test/java/gbench/sandbox/data/h2/H2db.java
@@ -411,21 +411,50 @@ public class H2db {
 	public static <N> String writeJson(final StringBuilder builder, final N node,
 			final Function<N, Iterable<N>> get_children, final BiConsumer<StringBuilder, N> pre_processor,
 			final BiConsumer<StringBuilder, N> post_processor) {
+		return writeJson(null, node, get_children, pre_processor, (sb, p) -> sb.append(null != p._1 ? "," : ""),
+				post_processor);
+	}
+
+	/**
+	 * 节点转换成Json
+	 * 
+	 * @param <N>            节点类型
+	 * @param builder        字符串构建器
+	 * @param node           树形元素节点
+	 * @param get_children   获取子节点
+	 * @param pre_processor  前处理 (sb:操作缓存,e:节点元素)->{}
+	 * @param processor      数据处理节点 (sb:操作缓存,(prev:前驱节点,c:当前节点):节点元素)->{}
+	 * @param post_processor 后处理 (sb:操作缓存,e:节点元素)->{}
+	 * @return json字符串
+	 */
+	public static <N> String writeJson(final StringBuilder builder, final N node,
+			final Function<N, Iterable<N>> get_children, final BiConsumer<StringBuilder, N> pre_processor,
+			final BiConsumer<StringBuilder, Tuple2<N, N>> processor,
+			final BiConsumer<StringBuilder, N> post_processor) {
 		final var sb = Optional.ofNullable(builder).orElse(new StringBuilder());
-		pre_processor.accept(sb, node);
+		if (null != pre_processor) {
+			pre_processor.accept(sb, node);
+		} // if
 		final var cc = get_children.apply(node);
 		if (cc != null) {
-			boolean flag = false;
-			for (final N c : cc) {
-				if (!flag) {
-					flag = true;
-				} else {
-					sb.append(",");
-				}
-				writeJson(sb, c, get_children, pre_processor, post_processor);
-			}
-		}
-		post_processor.accept(sb, node);
+			N prev = null;
+			if (processor != null) { // 存在中间处理器
+				for (final N c : cc) {
+					processor.accept(sb, Tuple2.of(prev, c));
+					writeJson(sb, c, get_children, pre_processor, processor, post_processor);
+					prev = c;
+				} // for
+			} else { // 不存在中间处理器
+				for (final N c : cc) {
+					writeJson(sb, c, get_children, pre_processor, null, post_processor);
+					prev = c;
+				} // for
+			} // if
+		} // if
+		if (null != post_processor) {
+			post_processor.accept(sb, node);
+		} // if
+
 		return sb.toString();
 	}
 
