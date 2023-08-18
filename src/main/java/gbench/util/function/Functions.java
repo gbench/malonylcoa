360240505d8770a938b87220b9a795200f241636
gbench <gbench@sina.com>
2023-08-18 08:26:38 +0800

    增加函数

--- src/main/java/gbench/util/function/Functions.java
+++ src/main/java/gbench/util/function/Functions.java
@@ -5,15 +5,16 @@ import java.util.function.*;
 public class Functions {
 
 	/**
+	 * argument-insensitive
 	 * 
-	 * @param <T>
-	 * @return
+	 * @param <T> 参数类型
+	 * @return 返回值类型
 	 */
 	public static <T> Function<T, T> identity() {
 		return new Function<T, T>() {
 
 			@Override
-			public T apply(T o) {
+			public T apply(final T o) {
 				return o;
 			}
 
@@ -22,5 +23,20 @@ public class Functions {
 				return "Functions.identity()";
 			}
 		};
+	}
+
+	/**
+	 * argument-insensitive <br>
+	 * 语法糖书写工具,辅助编译器推导入口参数类型,用于定义级联函数,比如 <br>
+	 * identity((Integer[]) null).andThen(INdarray::nd).andThen(INdarray::dupdata)
+	 * <br>
+	 * 就定义了一个函数类型:Function&ltInteger[],INdarray&ltInteger$gt&gt;函数
+	 * 
+	 * @param <T>         参数类型
+	 * @param placeholder 占位符
+	 * @return 返回值类型
+	 */
+	public static <T> Function<T, T> identity(final T placeholder) {
+		return identity();
 	}
 }
