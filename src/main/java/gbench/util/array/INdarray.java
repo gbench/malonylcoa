a053a3edebd575749161be6b58a65cac220aafd7
gbench <gbench@sina.com>
2023-08-19 15:33:52 +0800

    修改注释

--- src/main/java/gbench/util/array/INdarray.java
+++ src/main/java/gbench/util/array/INdarray.java
@@ -3330,17 +3330,18 @@ public interface INdarray<T> extends Comparable<INdarray<T>>, Iterable<T>, IStre
 	}
 
 	/**
-	 * 枢轴分类路径。
+	 * 枢轴分类路径。(为一个值 赋与 多个键,这个 多个键序列 或者说 键路径 就是pivotPath: <br>
+	 * pivotTable就是由pivotPath,于值value构成键(pivotPath)值(value)对儿结合: [(pivotPath,value)]
 	 * 
 	 * @param <CF>        分类函数类型 t->u
-	 * @param <T>         分类对象类型
-	 * @param <U>         分类路径的元素类型
+	 * @param <V>         分类对象类型
+	 * @param <K>         分类路径的元素类型
 	 * @param classifiers 枢轴分类器序列，分类策略。比如按照：[国家,性别,年代,研究方向】的枢轴分类，<br>
 	 *                    牛顿的 枢轴分类路径是 英国/男/现代/物理学家, 鲁迅:中国/男/现代/文学家, 李清照中国/女/南宋/诗人。
 	 * @return 分类结果
 	 */
-	static <CF extends Function<T, U>, T, U> INdarray<U> pivotPath(final Iterable<CF> classifiers, final T object) {
-		return INdarray.of(classifiers).fmap(f -> f.apply(object));
+	static <CF extends Function<V, K>, V, K> INdarray<K> pivotPath(final Iterable<CF> classifiers, final V value) {
+		return INdarray.of(classifiers).fmap(f -> f.apply(value));
 	}
 
 	Function<?, INdarray<Integer>> ndint = Types.cast((INdarray<Integer>) null);
