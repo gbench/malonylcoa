edab7eb324fdbd36e85f3b4e4a80be78141ace5b
gbench <gbench@sina.com>
2023-10-27 18:34:02 +0800

    修改注释

--- src/test/java/gbench/webapps/world/api/controller/ApiController.java
+++ src/test/java/gbench/webapps/world/api/controller/ApiController.java
@@ -4,11 +4,14 @@ import static gbench.util.lisp.IRecord.REC;
 import static java.time.LocalDateTime.now;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import gbench.util.data.MyDataApp;
 import gbench.util.lisp.IRecord;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("api")
@@ -31,14 +34,26 @@ public class ApiController {
 	 * @return IRecord
 	 */
 	@RequestMapping("sqlquery")
-	public IRecord sqlquery(final String sql) {
+	public Mono<IRecord> sqlquery(final String sql, final ServerHttpRequest req) {
 		final var ret = IRecord.REC("code", 0);
 		if (sql == null) {
+			req.getBody().subscribe(buffer -> {
+				final var bb = new byte[buffer.readableByteCount()];
+				buffer.read(bb);
+				DataBufferUtils.release(buffer);
+				try {
+					final var body = new String(bb, "utf8");
+					System.err.println(body);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
 			ret.add("data", this.dataApp.sqldframe("select * from t_maozedong limit 1"));
 		} else {
 			ret.add("data", this.dataApp.sqldframe(sql));
 		}
-		return ret;
+
+		return Mono.fromSupplier(() -> ret);
 	}
 
 	@Autowired
