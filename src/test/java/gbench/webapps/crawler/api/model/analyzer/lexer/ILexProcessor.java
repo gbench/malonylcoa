4f64977aab884ee3dd205f33b231c828f79bcb5d
gbench <gbench@sina.com>
2023-11-28 02:17:09 +0800

    增加文件

---
+++ src/test/java/gbench/webapps/crawler/api/model/analyzer/lexer/ILexProcessor.java
@@ -0,0 +1,61 @@
+package gbench.webapps.crawler.api.model.analyzer.lexer;
+
+/**
+ * 
+ * 语料库分词器:
+ * 通过符号获得词库中的单词信息，
+ * 是判断一个端字符是否构成也能够一个词库中某个词的前缀
+ * @author XUQINGHUA
+ *
+ */
+public interface ILexProcessor {
+    
+    /**
+     *  模块名称
+     * @return
+     */
+    public default String getName() {
+        final var clazz = this.getClass();
+        return clazz.getName();
+    }
+    
+    /**
+     * 
+     * @param name
+     */
+    public default void setName(String name) {
+        // do nothing
+    }
+    
+    /**
+     * 把符号转换成 词素
+     * @param symbol 符号
+     * @return 词素
+     */
+    public default Lexeme evaluate(String symbol) {
+        return new Lexeme(symbol,"unknown","unknown");
+    }
+    
+    /**
+     * 判断一个字符串line是否构成一个词库中的某个词的前缀
+     * @param line 前缀,待检测的字符串
+     * @return
+     */
+    public default boolean isPrefix(String line) {
+        return false;
+    }
+    
+    /**
+     * 终止连接
+     */
+    public default void uninitialize() {
+     // do nothing
+    }
+    
+    /**
+     * 初始化过程
+     */
+    public default void initialize() {
+        // do nothing
+    }
+}
---
+++ src/test/java/gbench/webapps/crawler/api/model/analyzer/lexer/LexemeAttributeImpl.java
@@ -0,0 +1,46 @@
+package gbench.webapps.crawler.api.model.analyzer.lexer;
+
+import org.apache.lucene.util.AttributeImpl;
+import org.apache.lucene.util.AttributeReflector;
+
+/**
+ * 
+ * 自定义属性
+ * @author gbench
+ *
+ */
+public class LexemeAttributeImpl extends AttributeImpl implements LexemeAttribute{
+
+    @Override
+    public void clear() {
+        this.lexeme = null;
+    }
+
+    @Override
+    public void reflectWith(final AttributeReflector reflector) {
+        reflector.reflect(LexemeAttribute.class, "lexeme", this.getLexeme());
+    }
+
+    @Override
+    public void copyTo(final AttributeImpl target) {
+        if(target==null)return;
+        if(target instanceof LexemeAttributeImpl ) {// 词素属性设置
+           ((LexemeAttributeImpl)target).setLexeme(lexeme);
+        } else {
+            // do nothing
+        }// 词素属性
+    }
+
+    @Override
+    public Lexeme getLexeme() {
+        return this.lexeme;
+    }
+    
+    @Override
+    public void setLexeme(final Lexeme lexeme) {
+       this.lexeme = lexeme;
+    }
+    
+    private Lexeme lexeme;// 词素属性
+
+}
