f9b17ec8f8ecaa1769fd3c2f943a6e59bfb06c12
gbench <gbench@sina.com>
2023-11-28 23:31:03 +0800

    修改注释

--- src/test/java/gbench/webapps/crawler/api/controller/ApiController.java
+++ src/test/java/gbench/webapps/crawler/api/controller/ApiController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import gbench.util.data.MyDataApp;
 import gbench.util.lisp.IRecord;
-import gbench.webapps.world.api.config.param.Param;
+import gbench.webapps.crawler.api.config.param.Param;
 import reactor.core.publisher.Mono;
 
 /**
