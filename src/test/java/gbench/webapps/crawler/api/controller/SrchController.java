ded9f45ea0cdef12273cd9fe80cfab8cd86cab88
gbench <gbench@sina.com>
2023-11-28 16:33:43 +0800

    修改注释

---
+++ src/test/java/gbench/webapps/crawler/api/controller/SrchController.java
@@ -0,0 +1,368 @@
+package gbench.webapps.crawler.api.controller;
+
+import org.apache.lucene.search.Query;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static gbench.util.data.DataApp.IRecord.FT;
+import static gbench.util.data.DataApp.IRecord.REC;
+import static gbench.util.data.DataApp.Tuple2.P;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import static java.text.MessageFormat.*;
+
+import gbench.util.data.DataApp.IRecord;
+import gbench.webapps.crawler.api.model.SrchModel;
+import gbench.webapps.crawler.api.model.SrchModel.FileSrchEngine;
+import gbench.webapps.crawler.api.model.analyzer.lexer.Trie;
+import gbench.webapps.crawler.api.model.srch.*;
+import gbench.webapps.crawler.api.model.srch.AbstractJdbcSrchEngine.PageQuery;
+import gbench.webapps.world.api.config.param.Param;
+
+/**
+ * 检索模型
+ */
+@RequestMapping("/srch")
+@RestController
+public class SrchController extends AbstractState<SrchController> {
+
+	/**
+	 * started callback
+	 * 
+	 * @return ApplicationRunner
+	 */
+	@Bean
+	ApplicationRunner onStarted() {
+		return args -> {
+			// 关键构造参数
+			final var prefix = "F:/slicef/ws/gitws/malonylcoa/src/test/";
+			final var corpusDir = FT("$0/java/gbench/webapps/crawler/api/model/data/corpus", prefix);
+			final var indexHome = "D:/sliced/tmp/crawler/index";
+			final var snapHome = "D:/sliced/tmp/crawler/snap";
+			this.initialize(indexHome, corpusDir, snapHome);
+		};
+	}
+
+	/**
+	 * 
+	 * @param indexHome
+	 * @param corpusDir
+	 * @param snapHome
+	 */
+	public void initialize(final String indexHome, final String corpusDir, final String snapHome) {
+		// 成员属性创建与初始化
+		this.srchModel = new SrchModel(indexHome, corpusDir, snapHome);
+		this.fileEngine = this.srchModel.new FileSrchEngine();
+		this.fileEngine.initialize();
+		this.state.set(REC());// 清空state
+		Trie.USE_RECURSIVE_GET_TRIE = false;// 使用LOOP模式的分词方式
+	}
+
+	/**
+	 * 关键词以前缀来进行检索
+	 * 
+	 * @param prefix 检索关键字
+	 * @param size   最大长度
+	 * @return 检索关键字
+	 */
+	@RequestMapping("keywords")
+	public IRecord keywords(final @Param String prefix, final @Param Integer size) {
+		synchronized (this.fileEngine) {
+			if (this.fileEngine.getKeywords().size() < 1)
+				this.fileEngine.refresh();
+		}
+		// 直接返回结果
+		return REC("code", 0, // 错误代码
+				"result", fileEngine.getKeywords().stream() // 数据结果
+						.filter(e -> e != null && e.startsWith((prefix == null ? "" : prefix).strip()))
+						.limit(size == null ? 10 : size)// 默认长度
+						.sorted((a, b) -> a.length() - b.length()).map(e -> REC(// 返回结果字段
+								"code", e, "label", e, "value", e))// getKeywords
+		);// REC
+	}
+
+	/**
+	 * 全文检索 <br>
+	 * 
+	 * 请求示例 http://localhost:6010/srch/lookup?keyword=存货
+	 * 
+	 * @param keyword 检索关键字
+	 * @return 检索关键字
+	 */
+	@RequestMapping("lookup")
+	public IRecord lookup(final @Param String keyword) {
+		return REC("code", 0, // 错误代码
+				"result", fileEngine.lookup(keyword == null ? "" : keyword, 50) // 数据结果
+		);// REC
+	}
+
+	/**
+	 * 全文检索:待有检索条件的项目检索
+	 * 
+	 * @param line 检索关键字
+	 * @return 检索关键字
+	 */
+	@RequestMapping("lookup2")
+	public IRecord lookup2(final @Param String line, final @Param String sessId, final @Param String agentId,
+			@Param Integer size) {
+		final var pageSize = size == null ? 10 : size;// 页面大小
+		final var ss = (line == null ? " " : line).split("\\s+");
+		final var rec = REC("+symbol*", format("*{0}*", ss.length > 0 ? ss[0] : "").trim(), "+file*",
+				format("*{0}*", ss.length > 1 ? ss[1] : "").trim());// rec
+
+		System.out.println(
+				format("\n#lookup2:\nline:【{0}】\n rec:【{1}】,sessionId:{2},agentId:{3}", line, rec, sessId, agentId));
+		final Query query = rec.tupleS().collect(SrchUtils.bool_query_clc);
+		final PageQuery pageQuery;
+		final Optional<PageData> optional;
+
+		// 设置各种key
+		final var pageQueryKey = format("${0}.${1}.${2}", agentId, "lookup2", "pageQuery");
+		final var lineKey = format("${0}.${1}.${2}", agentId, "lookup2", "line");
+		final var pageNumKey = format("${0}.${1}.${2}", agentId, "lookup2", "pagenum");
+		final var pageTotalKey = format("${0}.${1}.${2}", agentId, "lookup2", "pageTotal");
+
+		if (!line.equals(this.stateOfT(lineKey, Object.class))) {// 初次访问
+
+			pageQuery = fileEngine.getPageQuery(query);
+			pageQuery.initialize(10);
+			optional = pageQuery.getDataNoThrow();
+
+			// 程序状态
+			this.setState(line, lineKey);// 记录当前检索的请求
+			this.setState(pageQuery, pageQueryKey);
+			this.setState(1l, pageNumKey);// 记录行号
+			this.setState((long) Math.ceil(pageQuery.getTotalHits().value / pageSize) + 1, pageTotalKey);
+		} else { // 二次访问
+			pageQuery = this.stateOfT(pageQueryKey, PageQuery.class);
+			optional = pageQuery.nextPageNoThrow();
+
+			final var pagenum = this.getState(1l, pageNumKey);
+			final var pageTotal = this.getState(1l, pageTotalKey);
+
+			// 更新程序状态
+			this.setState(optional.isPresent() ? pagenum + 1 : pageTotal, pageNumKey);// 设置当前页
+		} // if
+
+		return REC("code", 0, // 错误代码
+				"pagenum", this.stateOfInteger(pageNumKey), "size",
+				optional.map(e -> e.getTotalHits().value).orElse(0l), "total", this.stateOfInteger(pageTotalKey),
+				"result", optional.map(e -> e.hitsStream()).orElse(Stream.of()) // 数据结果
+		);// REC
+	}
+
+	/**
+	 * 刷新关键词列表 <br>
+	 * 请求示例 http://localhost:6010/srch/refresh
+	 * 
+	 * @return 刷新列表
+	 */
+	@RequestMapping("refresh")
+	public IRecord refresh() {
+		es.execute(() -> this.fileEngine.refresh());// 执行刷新请求
+		return REC("code", 0, // 错误代码
+				"result", "刷新请求已经收到" // 数据结果
+		);// REC
+	}
+
+	/**
+	 * 全文检索
+	 * 
+	 * 请求示例
+	 * http://localhost:8081/poeny/srch/indexfiles?homeFile=C:/Users/xuqinghua/Desktop/史记.txt
+	 * 
+	 * @param homeFile 索引文件的起始位置
+	 * @return 检索关键字
+	 */
+	@RequestMapping("indexfiles")
+	@SuppressWarnings("unchecked")
+	public IRecord indexFiles(final String homeFile) {
+
+		if (homeFile == null) {
+			return REC("code", 2, // 错误代码
+					"result", MessageFormat.format("fileToBeIndexed 为空,不予执行索引", ""));// REC
+		}
+
+		// 索引线程
+		final Runnable runnable = () -> {// 创建线程 索引文件
+			this.srchModel.indexFiles(homeFile, rec -> { // 索引文件
+				final var tokens = rec.pathget("tokens", e -> (Stream<IRecord>) e);
+				final var files = rec.pathget("files", e -> (Stream<File>) e);
+				final var decomposeTime = rec.pathlng("decompose_time");
+				final var start = this.stateOfLong("indexfiles.start.time");
+				System.out.println(
+						MessageFormat.format("\n## {0} 索引完毕 !\n 索引词条:{1} 条 ,索引文件:{2} 个, 分词历时:{3} s , 索引总历时:{4} s ",
+								this.stateOfString("indexfiles.file.to.be.indexed"), tokens.count(), files.count(),
+								decomposeTime / 1000.0, // 分解结构
+								(System.currentTimeMillis() - start) / 1000.0 // 历时时间
+				));// println
+
+				this.state("indexfiles.running", false);// 清除运行状态
+				this.state("indexfiles.file.to.be.indexed", null);// 去除 索引文件
+				this.state("indexfiles.start.time", null);// 去除开始时间
+			});// 索引文件
+		};
+
+		// 仅当没有文件被索引的时候，开启文件索引功能
+		if (!this.stateOfBoolean("indexfiles.running")) {// 读取indexfiles的执行状态
+			this.state("indexfiles.running", true);// true 表示开始执行
+			this.state("indexfiles.file.to.be.indexed", homeFile);// true 表示开始执行
+			this.state("indexfiles.start.time", System.currentTimeMillis());// 记录开始时间
+
+			es.execute(runnable);// 开始执行文件索引
+
+			// 返回结果
+			return REC("code", 0, // 错误代码
+					"result", MessageFormat.format("开始索引:{0}！", homeFile));// REC
+		} else {
+			// 返回结果
+			return REC("code", 1, // 错误代码
+					"result",
+					MessageFormat.format("{0}正在被索引， 请稍后再予以申请！", this.stateOfString("indexfiles.file.to.be.indexed")));// REC
+		} // 线程文件
+	}
+
+	private FileSrchEngine fileEngine;// 搜索引擎
+	private SrchModel srchModel;// 搜索APP
+	private ExecutorService es = Executors.newFixedThreadPool(1);// 创建一个线程队列
+
+	/**
+	 * 启动函数
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SrchController.class, args);
+	}
+}
+
+/**
+ * 
+ * @author gbench
+ *
+ * @param <SELF> 子类的实际类型
+ */
+class AbstractState<SELF> {
+
+	/**
+	 * bool 类型的状态
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public boolean stateOfBoolean(String name) {
+		return this.state.get().bool(name);
+	}
+
+	/**
+	 * bool 类型的状态
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public int stateOfInteger(String name) {
+		return this.state.get().i4(name);
+	}
+
+	/**
+	 * bool 类型的状态
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public double stateOfDouble(String name) {
+		return this.state.get().dbl(name);
+	}
+
+	/**
+	 * bool 类型的状态
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public long stateOfLong(String name) {
+		return this.state.get().pathlng(name);
+	}
+
+	/**
+	 * bool 类型的状态
+	 * 
+	 * @param name
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> T stateOfT(String name, Class<T> clazz) {
+		return (T) this.state.get().get(name);
+	}
+
+	/**
+	 * bool 类型的状态
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public String stateOfString(String name) {
+		return this.state.get().str(name);
+	}
+
+	/**
+	 * 
+	 * @param <T>          返回值类型
+	 * @param defaultValue 默认值 或者 值类型
+	 * @param strings      键名序列
+	 * @return 属性的值
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> T getState(final T defaultValue, final Object... fields) {
+		final String key = Arrays.stream(fields).map(e -> e + "").collect(Collectors.joining("."));
+		final Class<T> tclass = defaultValue instanceof Class ? (Class<T>) defaultValue
+				: defaultValue == null ? (Class<T>) Object.class : (Class<T>) defaultValue.getClass();
+		final T t = this.stateOfT(key, tclass);
+		if (t != null)
+			return t;
+		else
+			return defaultValue instanceof Class ? null : defaultValue;
+	}
+
+	/**
+	 * 
+	 * @param <T>
+	 * @param value
+	 * @param strings
+	 * @return
+	 */
+	public <T> SELF setState(T value, String... strings) {
+		final String key = Arrays.stream(strings).collect(Collectors.joining("."));
+		return this.state(key, value);
+	}
+
+	/**
+	 * bool 类型的状态
+	 * 
+	 * @param name
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public SELF state(final String name, final Object value) {
+
+		// 重新设置新的状态信息
+		this.state.set(this.state.get().derive(Arrays.asList(P(name, value))));
+		return (SELF) this;
+	}
+
+	protected AtomicReference<IRecord> state = new AtomicReference<IRecord>(); // 系统状态
+
+}
---
+++ src/test/java/gbench/webapps/crawler/api/model/decompose/junit/JunitRule2.java
@@ -0,0 +1,30 @@
+package gbench.webapps.crawler.api.model.decompose.junit;
+
+import static gbench.util.data.DataApp.IRecord.FT;
+import static gbench.util.io.Output.println;
+
+import org.junit.jupiter.api.Test;
+
+import gbench.webapps.crawler.api.controller.SrchController;
+import gbench.webapps.crawler.api.model.SrchModel;
+
+public class JunitRule2 {
+
+	@Test
+	public void foo() {
+		final var prefix = "F:/slicef/ws/gitws/malonylcoa/src/test/";
+		final var fileHome = FT("$0/java/gbench/webapps/crawler/api/model/data/docs/rules", prefix);
+		final var corpusDir = FT("$0/java/gbench/webapps/crawler/api/model/data/corpus", prefix);
+		final var indexHome = "D:/sliced/tmp/crawler/index";
+		final var snapHome = "D:/sliced/tmp/crawler/snap";
+		final var srchModel = new SrchModel(indexHome, corpusDir, snapHome);
+		srchModel.indexFiles(fileHome, e -> {
+			println(e);
+		});
+		final var srchCtrl = new SrchController();
+		srchCtrl.initialize(indexHome, corpusDir, snapHome);
+		println("----------------------");
+		println(srchCtrl.lookup("存货"));
+	}
+
+}
