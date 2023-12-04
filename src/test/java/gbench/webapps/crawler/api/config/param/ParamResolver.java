18a80e8d07d40d03634c977d48e00e299cbf48a6
gbench <gbench@sina.com>
2023-12-05 05:50:34 +0800

    修改注释

--- src/test/java/gbench/webapps/crawler/api/config/param/ParamResolver.java
+++ src/test/java/gbench/webapps/crawler/api/config/param/ParamResolver.java
@@ -49,6 +49,9 @@ public class ParamResolver extends AbstractMessageReaderArgumentResolver {
 		}
 	}
 
+	/**
+	 * 参数处理
+	 */
 	@Override
 	public Mono<Object> resolveArgument(final MethodParameter methodParameter, final BindingContext bindingContext,
 			final ServerWebExchange serverWebExchange) {
@@ -60,6 +63,7 @@ public class ParamResolver extends AbstractMessageReaderArgumentResolver {
 		final var type1 = methodParameter.getParameterType();
 		final var type = type0 == Object.class ? type1 : type0;
 		final var qps = serverWebExchange.getRequest().getQueryParams();
+
 		return serverWebExchange.getFormData().map(data -> {
 			final var ll = Optional.ofNullable(data.get(name)).orElse(qps.get(name));
 			final Optional<Object> opt = Optional.ofNullable(ll != null && ll.size() > 0 ? ll.get(0) : null)
