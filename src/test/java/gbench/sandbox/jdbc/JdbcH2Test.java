25cb1cde1983d5c2850bde998dbcb600a39df1e4
gbench <gbench@sina.com>
2023-12-23 19:17:34 +0800

    修改注释

--- src/test/java/gbench/sandbox/jdbc/JdbcH2Test.java
+++ src/test/java/gbench/sandbox/jdbc/JdbcH2Test.java
@@ -4,8 +4,11 @@ import static gbench.sandbox.data.h2.H2db.imports;
 import static gbench.util.io.Output.println;
 import static gbench.util.jdbc.kvp.IRecord.REC;
 
+import java.util.function.Consumer;
+
 import org.junit.jupiter.api.Test;
 
+import gbench.util.jdbc.kvp.IRecord;
 import gbench.sandbox.data.h2.H2db;
 import gbench.util.data.MyDataApp;
 import gbench.util.jdbc.IJdbcApp;
@@ -28,15 +31,15 @@ public class JdbcH2Test {
 		final var tables = "t_company,t_product,t_company_product,t_customer,t_coa,t_accts,t_journal,t_bksys"
 				.split("[,]+"); // 基础数据表
 		final var cp_sql = "select * from t_company_product where company_id=##cid"; // 公司产品
+		final Consumer<? super IRecord> attrs_f = e -> e.compute("attrs", H2db::asMap);
+
 		// 数据初始导入
 		new MyDataApp(h2_rec.toMap()).withTransaction(imports(tables));
 		// 数据操作
 		jdbcApp.withTransaction(sess -> {
 			println("all tables", sess.sql2dframe("#getAllTables"));
 			println("t_product", sess.sql2dframe("select * from t_product limit ##cnt", "cnt", 2));
-			println("t_company_product", sess.sql2dframe(cp_sql, "cid", 1).forEachByRow(e -> {
-				e.compute("attrs", H2db::asMap);
-			}));
+			println("t_company_product", sess.sql2dframe(cp_sql, "cid", 1).forEachByRow(attrs_f));
 		});
 	}
 
