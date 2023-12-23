dc45cf47863065cc6341dc9e20e7ab2acf69f725
gbench <gbench@sina.com>
2023-12-23 13:04:07 +0800

    修改注释

--- src/test/java/gbench/sandbox/jdbc/JdbcMysqlTest.java
+++ src/test/java/gbench/sandbox/jdbc/JdbcMysqlTest.java
@@ -25,27 +25,7 @@ import gbench.util.jdbc.kvp.IRecord;
 public class JdbcMysqlTest {
 
 	/**
-	 * mysql 数据库操作接口<br>
-	 * MysqlDatabase <br>
-	 * <br>
-	 * mysql> show variables like '%time_zone%' <br>
-	 * system_time_zone | China Standard Time <br>
-	 * <br>
-	 * DST: Daylight Saving Time中文名叫“夏令时”，一般在天亮早的夏季人为将时间调快一小时，<br>
-	 * 可以使人早起早睡，减少照明量，以充分利用光照资源，从而节约照明用电。中国1986-1991年实行夏令时，1992年废除。 <br>
-	 * GMT-8是东八区，北京时间和东八区一致。 <br>
-	 * CST: China Standard Time（老外认为有其他含义，中国就这个缩写），中国标准时。
-	 * 中国1986-1991年实行夏令时，1992年废除。<br>
-	 * Asia/Shanghai是已地区命名的地区标准时，在中国叫CST。<br>
-	 * GMT：Greenwich Mean Time，格林威治标准时，地球每15°经度 被分为一个时区，共分为24个时区，相邻时区相差一小时；例:
-	 * 中国北京位于东八区。 <br>
-	 * 1992年以后，在中国，GMT+8和Asia/Shanghai是一样的时间，1986-1991之间，夏天会有一小时时差。 <br>
-	 * 这就是 为何这里把时区写成Asia/Shanghai而不写成 GMT+8 (GMT%2B8) 的原因，因为 中国在 1986-1991
-	 * 使用夏令时。<br>
-	 * 时间 Asia/Shanghai '1989-05-02 00:18:00' 在 GMT+8 中 为 '1989-05-02 01:18:00'
-	 * 
-	 * @author gbench
-	 *
+	 * 数据库配置
 	 */
 	@JdbcConfig(url = "jdbc:h2:mem:malonylcoa;MODE=MYSQL;DB_CLOSE_DELAY=-1;database_to_upper=false;", user = "root", password = "123456")
 	interface JdbcApp extends IMySQL { // 数据接口
@@ -138,6 +118,7 @@ public class JdbcMysqlTest {
 		// 创造一个IJdbcApp接口应用
 		final var sqlfile = "F:/slicef/ws/gitws/malonylcoa/src/test/java/gbench/sandbox/jdbc/sqls/mysql_test.sql";
 		final var jdbcApp = IJdbcApp.newDBInstance(() -> nspp(sqlfile), JdbcApp.class);
+
 		this.run(jdbcApp);
 	}
 
@@ -151,9 +132,29 @@ public class JdbcMysqlTest {
 		final var dbname = String.format("malonylcoa_%s", "bar"); // 更换一个数据库
 		final var url = String.format("jdbc:h2:mem:%s1;MODE=MYSQL;DB_CLOSE_DELAY=-1;database_to_upper=false;", dbname);
 		final var driver = "org.h2.Driver";
-		final var h2_rec_1 = IRecord.REC("url", url, "driver", driver, "user", "root", "password", "123456");
+		final var h2_rec_1 = REC("url", url, "driver", driver, "user", "root", "password", "123456");
 		final var jdbcApp = IJdbcApp.newNsppDBInstance(sqlfile, JdbcApp.class, h2_rec_1);
+
 		this.run(jdbcApp);
+	}
+
+	/**
+	 * 使用指定配置来创建
+	 */
+	@Test
+	public void quz() {
+		// 创造一个IJdbcApp接口应用
+		final var sqlfile = "F:/slicef/ws/gitws/malonylcoa/src/test/java/gbench/sandbox/jdbc/sqls/mysql_test.sql";
+		final var dbname = "hitler"; // 更换一个数据库
+		final var url = String.format("jdbc:mysql://127.0.0.1:3309/%s?serverTimezone=UTC", dbname);
+		final var driver = "org.h2.Driver";
+		final var mysql_rec = REC("url", url, "driver", driver, "user", "root", "password", "123456");
+		final var jdbcApp = IJdbcApp.newNsppDBInstance(sqlfile, JdbcApp.class, mysql_rec);
+
+		jdbcApp.withTransaction(sess -> {
+			println("all tables", sess.sql2dframe("#getAllTables"));
+			println("t_coa", sess.sql2dframe("select * from t_coa limit 5"));
+		});
 	}
 
 }
