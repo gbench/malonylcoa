ebc84536ec30e88c35a340c079b70008916b8354
gbench <gbench@sina.com>
2023-12-23 09:34:13 +0800

    修改注释

--- src/main/java/gbench/util/math/FinanceMaths.java
+++ src/main/java/gbench/util/math/FinanceMaths.java
@@ -25,18 +25,40 @@ public class FinanceMaths {
 	/**
 	 * 实际利率寒素参见excelRATE函数<br>
 	 * 
-	 * @param nper  Required. The total number of payment periods in an annuity.
-	 * @param pmt   Required. The payment made each period and cannot change over
-	 *              the life of the annuity. Typically, pmt includes principal and
-	 *              interest but no other fees or taxes. If pmt is omitted, you must
-	 *              include the fv argument.
-	 * @param pv    Required. The present value — the total amount that a series of
-	 *              future payments is worth now.
-	 * @param fv    Optional. The future value, or a cash balance you want to attain
-	 *              after the last payment is made. If fv is omitted, it is assumed
-	 *              to be 0 (the future value of a loan, for example, is 0). If fv
-	 *              is omitted, you must include the pmt argument.
-	 * @param type  Optional. The number 0 or 1 and indicates when payments are due.
+	 * @param nper Required. The total number of payment periods in an annuity.
+	 * @param pmt  Required. The payment made each period and cannot change over the
+	 *             life of the annuity. Typically, pmt includes principal and
+	 *             interest but no other fees or taxes. If pmt is omitted, you must
+	 *             include the fv argument.
+	 * @param pv   Required. The present value — the total amount that a series of
+	 *             future payments is worth now.
+	 * @param fv   Optional. The future value, or a cash balance you want to attain
+	 *             after the last payment is made. If fv is omitted, it is assumed
+	 *             to be 0 (the future value of a loan, for example, is 0). If fv is
+	 *             omitted, you must include the pmt argument.
+	 * @return Double 非收敛返回null,type 默认为0:期末产生收入
+	 */
+	public static Double rate(final Number nper, final Number pmt, final Number pv, final Number fv) {
+		return rate(nper, pmt, pv, fv, 0, null);
+	}
+
+	/**
+	 * 实际利率寒素参见excelRATE函数<br>
+	 * 
+	 * @param nper Required. The total number of payment periods in an annuity.
+	 * @param pmt  Required. The payment made each period and cannot change over the
+	 *             life of the annuity. Typically, pmt includes principal and
+	 *             interest but no other fees or taxes. If pmt is omitted, you must
+	 *             include the fv argument.
+	 * @param pv   Required. The present value — the total amount that a series of
+	 *             future payments is worth now.
+	 * @param fv   Optional. The future value, or a cash balance you want to attain
+	 *             after the last payment is made. If fv is omitted, it is assumed
+	 *             to be 0 (the future value of a loan, for example, is 0). If fv is
+	 *             omitted, you must include the pmt argument.
+	 * @param type Optional. The number 0 or 1 and indicates when payments are due.
+	 *             1) 0 or omitted(null) : At the end of the period <br>
+	 *             2) 1: At the beginning of the period <br>
 	 * @return Double 非收敛返回null
 	 */
 	public static Double rate(final Number nper, final Number pmt, final Number pv, final Number fv,
@@ -59,10 +81,14 @@ public class FinanceMaths {
 	 *              to be 0 (the future value of a loan, for example, is 0). If fv
 	 *              is omitted, you must include the pmt argument.
 	 * @param type  Optional. The number 0 or 1 and indicates when payments are due.
+	 *              1) 0 or omitted(null) : At the end of the period <br>
+	 *              2) 1: At the beginning of the period <br>
 	 * @param guess Guess Optional. Your guess for what the rate will be. <br>
 	 *              If you omit guess, it is assumed to be 10 percent. <br>
 	 *              If RATE does not converge, try different values for guess. RATE
-	 *              usually converges if guess is between 0 and 1
+	 *              <br>
+	 *              usually converges if guess is between 0 and 1 <br>
+	 * 
 	 * @return Double 非收敛返回null
 	 */
 	public static Double rate(final Number nper, final Number pmt, final Number pv, final Number fv, final Number type,
