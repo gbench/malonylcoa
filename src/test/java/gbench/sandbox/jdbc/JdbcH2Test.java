38d07fd2518787268ea920097b393876c451de7d
gbench <gbench@sina.com>
2023-12-24 08:50:12 +0800

    修改注释

--- src/test/java/gbench/sandbox/jdbc/JdbcH2Test.java
+++ src/test/java/gbench/sandbox/jdbc/JdbcH2Test.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -18,6 +19,7 @@ import gbench.sandbox.data.h2.H2db;
 import gbench.util.data.MyDataApp;
 import gbench.util.jdbc.IJdbcApp;
 import gbench.util.jdbc.IMySQL;
+import gbench.util.jdbc.function.ExceptionalConsumer;
 
 import static gbench.sandbox.data.h2.H2db.imports;
 import static gbench.util.io.Output.println;
@@ -28,6 +30,9 @@ import static gbench.util.jdbc.sql.SQL.sql;
 import static java.time.LocalDateTime.now;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.summarizingDouble;
+import static java.util.stream.Stream.iterate;
+
+import java.time.LocalDateTime;
 
 /**
  * H2数据库示例
@@ -64,7 +69,8 @@ public class JdbcH2Test {
 	 * @param stores     收获地址（仓库）
 	 * @return IRecord
 	 */
-	final static IRecord buildOrder(final DFrame companydfm, final DFrame cpdfm, final String[] stores) {
+	final static IRecord buildOrder(final DFrame companydfm, final DFrame cpdfm, final String[] stores,
+			final LocalDateTime ldt) {
 
 		final var rnd = new Random(); // 随机值
 		final var parts = sample(companydfm.rowS(), 2); // 订单各方
@@ -78,10 +84,10 @@ public class JdbcH2Test {
 				.map(e -> e.add("price", e.dbl("price") * rnd.nextDouble(1d), "quantity", 1 + rnd.nextInt(100))) // 价格调整
 				.map(e -> e.add("amount", e.dbl("price") * e.dbl("quantity"))).toList(); // 订单行项目
 		final var amount = items.stream().collect(summarizingDouble(e -> e.dbl("price") * e.dbl("quantity"))).getSum(); // 订单金额
-		final var rb = rb("#id,name,shipper,receiver,receive_address,amount,details,create_time"); // 订单结构
+		final var rb = rb("name,shipper,receiver,receive_address,amount,details,create_time"); // 订单结构
 		final var details = REC("flag", false, "amount", amount, "items", items); // 订单详情
-		final var t_order = rb.get(1, products.get(0).get("name"), shipper.get("id"), receiver.get("id"),
-				receive_address, amount, details, now()); // 订单记录
+		final var t_order = rb.get(products.get(0).get("name"), shipper.get("id"), receiver.get("id"), receive_address,
+				amount, details, ldt); // 订单记录
 
 		return t_order;
 	}
@@ -89,25 +95,24 @@ public class JdbcH2Test {
 	/**
 	 * 准备公司产品
 	 * 
-	 * @param companydfm 公司数据
-	 * @param productdfm 产品数据
+	 * @param cs 公司数据
+	 * @param ps 产品数据
 	 * @return 公司产品
 	 */
-	public List<IRecord> buildCPs(final DFrame companydfm, final DFrame productdfm) {
-		final var data = new LinkedList<IRecord>();
-		for (final var c : companydfm.rows()) {
-			for (final var p : productdfm.rows()) {
+	public List<IRecord> buildCPs(final DFrame cs, final DFrame ps) {
+		final var cps = new LinkedList<IRecord>();
+		for (final var c : cs.rows()) {
+			for (final var p : ps.rows()) {
 				final var cid = c.get("id"); // 公司id
 				final var pid = p.get("id"); // 产品id
-				final var cprb = rb("#id,company_id,product_id,attrs,create_time,update_time");
+				final var cp_rb = rb("company_id,product_id,attrs,create_time,update_time");
 				final var attrs = REC("url", "C:/Users/Administrator/Pictures/fumarate/sale.jpg", //
 						"name", p.get("name"), "price", p.get("price"), "price_striked", p.get("price_striked"));
-				final var cp = cprb.get(1, cid, pid, attrs, now(), now());
-
-				data.add(cp.remove("#id"));
+				final var cp = cp_rb.get(cid, pid, attrs, now(), now());
+				cps.add(cp);
 			}
 		}
-		return data;
+		return cps;
 	}
 
 	/**
@@ -117,10 +122,58 @@ public class JdbcH2Test {
 	 * @param size 分组长度
 	 * @return 数据分组
 	 */
-	public Map<Integer, List<IRecord>> partitions(final List<IRecord> data, final int size) {
+	public static Map<Integer, List<IRecord>> partitions(final List<IRecord> data, final int size) {
+		return partitions(data.stream(), size);
+	}
+
+	/**
+	 * 数据分组
+	 * 
+	 * @param dataS 数据列表
+	 * @param size  分组长度
+	 * @return 数据分组
+	 */
+	public static Map<Integer, List<IRecord>> partitions(final Stream<IRecord> dataS, final int size) {
 		final var ar = new AtomicInteger();
-		final var grouped_sqls = data.stream().collect(groupingBy(e -> ar.getAndIncrement() / size));
-		return grouped_sqls;
+		final var parts = dataS.collect(groupingBy(e -> ar.getAndIncrement() / size));
+		return parts;
+	}
+
+	/**
+	 * create table sql
+	 * 
+	 * @param name  表名
+	 * @param proto 数据原型
+	 * @return create table sql
+	 */
+	public static String ctsql(final String name, final IRecord proto) {
+		return sql(name, REC("#id", 1).add(proto)).ctsqls(true).get(2);
+	}
+
+	/**
+	 * 数据插入
+	 * 
+	 * @param name  数据表
+	 * @param proto 数据原型
+	 * @return 数据插入
+	 */
+	public static String insql(final String name, final List<IRecord> data) {
+		return sql(name, data).insql();
+	}
+
+	/**
+	 * 
+	 * @param <K>
+	 * @param <V>
+	 * @param partitions
+	 * @param cs
+	 * @throws Exception
+	 */
+	public static <K, V> void batch_execute(final Map<K, V> partitions, final ExceptionalConsumer<V> cs)
+			throws Exception {
+		for (final var partition : partitions.entrySet()) { // 重新设置公司产品
+			cs.accept(partition.getValue());
+		}
 	}
 
 	@Test
@@ -136,6 +189,9 @@ public class JdbcH2Test {
 				.split("[,]+"); // 基础数据表
 		final var cp_sql = "select * from t_company_product where company_id=##cid"; // 公司产品
 		final var stores = "北京,天津,重庆,上海,广州,深圳".split("[,]+"); // 仓库地址
+		final var top10 = "select * from ##tbl";
+		final var size = 1000; // 数据量
+		final var batch_size = 10; // 批次大小
 
 		// 数据初始导入
 		new MyDataApp(h2_rec.toMap()).withTransaction(imports(tables)); // 导入订单数据
@@ -146,31 +202,34 @@ public class JdbcH2Test {
 			println("t_company_product", sess.sql2dframe(cp_sql, "cid", 1).forEachByRow(processor("attrs")));
 			println(sess.sql2dframe("#trialBalanceForH2", "bksys_id", 1)); // 试算平衡表
 
-			final var companydfm = sess.sql2dframe("select * from t_company"); // 公司信息
-			final var productdfm = sess.sql2dframe("select id,name,price,price_striked from t_product cp"); // 产品信息
+			final var cs = sess.sql2dframe("select * from t_company"); // 公司信息
+			final var ps = sess.sql2dframe("select id,name,price,price_striked from t_product cp"); // 产品信息
 			final var cp_name = "t_company_product"; // 公司产品表名
-			final var partitions = this.partitions(this.buildCPs(companydfm, productdfm), 100); // 公司产能品数据
-			final var proto = partitions.entrySet().iterator().next().getValue().getFirst();
+			final var or_name = "t_order"; // 订单名称
+			final var cp_partitions = partitions(this.buildCPs(cs, ps), batch_size); // 公司产能品数据
+			final var cp_proto = cp_partitions.entrySet().iterator().next().getValue().getFirst();
 
+			// 公司产品数据
 			sess.sqlexecute("drop table t_company_product"); // 移除公司产品数据
-			sess.sql2execute(println(sql(cp_name, proto).ctsqls(true).get(2))); // 创建数据表
-			for (final var partition : partitions.entrySet()) { // 重新设置公司产品
-				println("ids", sess.sql2execute(println(sql(cp_name, partition.getValue()).insql())));// 插入数据
-			}
+			sess.sql2execute(println(ctsql(cp_name, cp_proto))); // 创建数据表
+			batch_execute(cp_partitions, partition -> { // 公司产品
+				println("cp ids", sess.sql2execute(println(insql(cp_name, partition))));// 插入数据
+			}); // 重新设置公司产品
 
-			final var cpdfm = sess.sql2dframe("select * from t_company_product").forEachByRow(processor("attrs"))
-					.fmapByRow(e -> e.path2rec("attrs").add(e.filter("id,company_id,product_id")));
-			final var order_proto = buildOrder(companydfm, cpdfm, stores); // 数据原型 )
+			// 订单数据处理
+			final var cps = sess.sql2dframe("select * from ##tbl", "tbl", cp_name).forEachByRow(processor("attrs"))
+					.fmapByRow(e -> e.path2rec("attrs").add(e.filter("id,company_id,product_id"))); // 公司产品
+			final Supplier<IRecord> os = () -> buildOrder(cs, cps, stores, now()); // 订单生成函数,order supplier
+			final var o_proto = os.get(); // 数据原型
+			final var o_partitions = partitions(iterate(os.get(), i -> os.get()).limit(size), batch_size); // 公司产能品数据
+			sess.sql2execute(println(ctsql(or_name, o_proto))); // 创建数据表
+			batch_execute(o_partitions, partition -> { // 订单数据
+				println("order ids", sess.sql2execute(println(insql(or_name, partition))));// 插入数据
+			}); // 重新设置公司产品
 
-			sess.sql2execute(println(sql("t_order", order_proto).ctsqls(true).get(2))); // 创建数据表
-			for (int i = 0; i < 10; i++) { // 创建数据表
-				sess.sql2execute("#addOrder", buildOrder(companydfm, cpdfm, stores)); // 创建订单
-			} // for
-			
-			println("t_order", sess.sql2dframe("select * from t_order") //
-					.forEachByRow(processor("details"))); // 查看订单记录
-			println("t_company_product", sess.sql2dframe("select * from ##tbl limit 10", "tbl", cp_name) //
-					.forEachByRow(processor("attrs"))); // 查看订单记录
+			// 数据查看
+			println("t_order", sess.sql2dframe(top10, "tbl", or_name).forEachByRow(processor("details"))); // 查看订单记录
+			println("t_company_product", sess.sql2dframe(top10, "tbl", cp_name).forEachByRow(processor("attrs"))); // 查看订单记录
 		});
 	}
 
