510d50644c763e229971e52b25f1bb1d023a5a70
gbench <gbench@sina.com>
2024-01-11 00:38:31 +0800

    增加final标记

--- src/test/java/gbench/webapps/mymall/ui/src/comps/bcomp/BComp.js
+++ src/test/java/gbench/webapps/mymall/ui/src/comps/bcomp/BComp.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import $ from "jquery";
 import { mapGetters } from "vuex";
 
 const BComp = {
@@ -9,7 +11,9 @@ const BComp = {
      * @returns 
      */
     data() {
-        return {};
+        return {
+            fileurl: ""
+        };
     },
 
     /**
@@ -27,6 +31,26 @@ const BComp = {
          * Getters 数据
          */
         ...mapGetters("BCompStore", ["name"]),
+    },
+
+    methods: {
+        /**
+         * 
+         * @param {*} event 
+         */
+        on_file_change(event) {
+            const url = "/h5/api/upload";
+            const formData = new window.FormData();
+            const file = event.target.files[0];
+            formData.append("file", file);
+            axios.post(url, formData, {
+                headers: { "Content-Type": "multipart/form-data" }
+            }).then(e => {
+                const data = e.data;
+                this.fileurl = `/h5/api/readfile?file=${data.path}`;
+                // alert(JSON.stringify(data));
+            });
+        }
     }
 
 };
