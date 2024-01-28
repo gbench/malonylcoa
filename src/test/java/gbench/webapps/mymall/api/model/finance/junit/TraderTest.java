d24ac3ae3599db151b5aa35cb5194d0eea25f9ab
gbench <gbench@sina.com>
2024-01-28 23:00:39 +0800

    修改注释

--- src/test/java/gbench/webapps/mymall/api/model/finance/junit/TraderTest.java
+++ src/test/java/gbench/webapps/mymall/api/model/finance/junit/TraderTest.java
@@ -7,7 +7,11 @@ import static gbench.util.jdbc.Jdbcs.h2_json_processor;
 import static gbench.util.jdbc.kvp.IRecord.M;
 import static gbench.webapps.mymall.api.model.finance.Finances.*;
 
+import java.util.Collections;
+import java.util.stream.Stream;
+
 import gbench.util.jdbc.kvp.IRecord;
+import gbench.util.tree.Node;
 import gbench.webapps.mymall.api.model.finance.trading.FinTraderBuilder;
 
 /**
@@ -24,15 +28,15 @@ public class TraderTest {
 	public void foo() throws InterruptedException {
 		final var trader = FinTraderBuilder.build("id", 1);
 		final var invt = trader.getInventory();
-		final var times = 10; // 出入库次数
+		final var times = 20; // 出入库次数
 		for (int i = 0; i < times; i++) { // 模拟数据
-			final var flag = Math.random() > 0.5;// 随机生成出入库单
-			final var bill = flag //
+			final var is_invoice = i >= times / 2; // 随机生成出入库单
+			final var bill = is_invoice //
 					? trader.saleOrder(2, null) // 卖出订单
 					: trader.purchaseOrder(2, null); // 买入订单
 			println(bill);
 			Thread.sleep(1000);
-			if (flag) {
+			if (is_invoice) {
 				trader.invoice(bill.dfm("details/items"), 1, bill.lng("id"));
 			} else {
 				trader.receipt(bill.dfm("details/items"), 1, bill.lng("id"));
@@ -42,13 +46,21 @@ public class TraderTest {
 		println("bldfm", invt.bills().forEachBy(h2_json_processor("details")));
 		println("fifo", invt.fifo()); // 出入库信息
 		final var root = invt.fifo().pivotTable0("id,drcr,bill_id", aa -> M( //
-				"bill_id", aa.get(0).lng("bill_id"), // 数量
+				"bill_id", aa.get(0).i4("bill_id"), //
 				"qty", dqsum(aa), // 数量
-				"price", aa.get(0).ldt("price"), // 价格
+				"price", aa.get(0).dbl("price"), // 价格
 				"time", aa.get(0).ldt("time") // 时间
 		));
 		println("tree");
 		root.treeNode().forEach(e -> {
+			if (e.getLevel().equals(3)) {
+				Collections.sort(e.getChildren(), (a, b) -> { // 低于drcr层按照bill_id进行排序
+					final var aa = Stream.of(a, b) //
+							.map(Node::getName).map(IRecord.obj2dbl()) //
+							.toArray(Double[]::new);
+					return aa[0].compareTo(aa[1]);
+				});
+			}
 			final var ident = " | ".repeat(e.getLevel() - 1);
 			final var name = e.getName();
 			final var value = e.attrval(IRecord::REC);
