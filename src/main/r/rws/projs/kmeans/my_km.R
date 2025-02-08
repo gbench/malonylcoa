e8d9b908ddc29696e8bec1824208fa9faf44d336
gbench <gbench@sina.com>
2025-02-09 02:47:31 +0800

    补充分类器

--- src/main/r/rws/projs/kmeans/my_km.R
+++ src/main/r/rws/projs/kmeans/my_km.R
@@ -20,7 +20,7 @@ if (!require(purrr, quietly = TRUE)) {
 #         cluster_ids: 聚类中心id, 即数据点的分类编码, cluster_id就是未给予正式的命名的概念或者说
 #         是机器分析出的概念，一旦为cluster_id即分类id进行了命名,给出了带有人类文化&经验色彩的名称,
 #         cluster_id或者说分类id就就变成了我们通常使用的概念了，它的意义将由该分类的下的数据元素的
-#         共性所承载}
+#         共性所承载,classifier:数据分类器}
 km <- function(data, k, eps = 0.01) {
   # -------------------------------------------------------
   # 数据类型验证
@@ -39,15 +39,18 @@ km <- function(data, k, eps = 0.01) {
   n <- nrow(data) # 数据长度
 
   # 计算 data 与 特定 点之间的距离,p:点的各个维度坐标的向量
-  d <- function(p) {
+  # @param data 数据点集合
+  # @param p 指定数据点
+  d <- function(data, p) {
     apply(data, 1, compose(sqrt, sum, \(x) (x - p)^2))
   }
 
   # cluster_ids的本质是一个分类器(classifier), 即将拥有着共同的聚类中心的点视为同一个类别
+  # @param data 数据点集合
   # @param centeroids k个聚类中心的索引坐标 (行:点索引编号从1到k, 列:各个维度坐标)
   # @return 获取聚类中点id索引号：分类标记
-  cluster_ids <- function(centeroids) {
-    apply(centeroids, 1, d) |> # 按行统计 ,计算各个点的距离, 结果为矩阵：(行data点编号,列centeroids点编号）
+  cluster_ids <- function(data, centeroids) {
+    apply(centeroids, 1, \(p) d(data, p)) |> # 按行统计 ,计算各个点的距离, 结果为矩阵：(行data点编号,列centeroids点编号）
       apply(1, which.min) # 按照行统计， 找出各数据data点与centeroids各点距离中最小的centeriods索引点编号作为cluster_id
   }
 
@@ -104,7 +107,7 @@ km <- function(data, k, eps = 0.01) {
 
   repeat { # 一直计算到中心点收敛到指定的误差范围之内eps：当前点.centeroids与先前点centeroids之间的各个维度坐标的差绝对值小于eps
     # 求出各个分类的样本的均值:作为准聚类中心点，更新聚类中心点
-    .centeroids <- split(data, cluster_ids(centeroids)) |> # 计算各个数据点的聚类id
+    .centeroids <- split(data, cluster_ids(data, centeroids)) |> # 计算各个数据点的聚类id
       lapply(\(x) apply(x, 2, mean)) |> # 介个每个分组的各个维度的坐标平均值
       Reduce(x = _, f = rbind, init = data.frame()) # 结果合并为数据框,组合成当前中心点.centeroids,即准中心点
 
@@ -118,9 +121,10 @@ km <- function(data, k, eps = 0.01) {
     }
   } # repeat
 
-  classifier <- cluster_ids # 将cluster_ids 作为数据点的分类器
+  # 分类器
+  classifier <- function(data) cluster_ids(data, centeroids) # 将cluster_ids 作为数据点的分类器
   # 返回结果，{centeroids: 聚类中心点, cluster_ids: 聚类中心点ids, 其实聚类中心id就是数据点的分类编码}
-  list(centeroids = structure(centeroids, names = names(data)), cluster_ids = classifier(centeroids))
+  list(centeroids = structure(centeroids, names = names(data)), cluster_ids = classifier(data), classifier = classifier)
 }
 
 # ---------------------------------------------------------------------
@@ -133,6 +137,8 @@ print(km.res) # 打印结果
 
 # 将聚类结果添加到原始数据中
 data$Cluster <- as.factor(km.res$cluster_ids) # 加入聚类中心点
+# 使用分类器进行分类
+print(km.res$classifier(iris[, -5]))
 
 # ---------------------------------------------------------------------
 # 数据绘图
