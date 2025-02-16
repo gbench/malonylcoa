ba0fe8eea38597b644ca27ae2cafb659aad51ecc
gbench <gbench@sina.com>
2025-02-16 15:43:51 +0800

    修改注释

--- src/main/r/rws/projs/kmeans/my_km.R
+++ src/main/r/rws/projs/kmeans/my_km.R
@@ -88,14 +88,15 @@ km <- function (data, k, eps = 0.01) {
   flag <- F # 聚类中心点的生成方式，是否使用随机生成的中心点， True 随机方式, False非随机方式
   
   #' 一直计算到中心点收敛到指定的误差范围之内eps：当前点.cs与先前点cs之间的各个维度坐标的差绝对值小于eps
-  #' @param cs 假定中心点集合
+  #' @param cs 假定中心点集合:矩阵类型
   #' @return 调整后的中心点集合:矩阵类型 
   loop <- function (cs = centeroids_gen(flag)) { 
     # 求出各个分类的样本的均值:作为准聚类中心点，更新聚类中心点
     .cs <- split(data, cluster_ids(data, cs)) |> # 计算各个数据点的聚类id
-      lapply(\(x) apply(x, 2, mean)) |> # 介个每个分组的各个维度的坐标平均值
+      lapply(\(x) apply(x, 2, mean)) |> # 计算每个分组的在各个维度上的坐标平均值
       Reduce(x = _, f = \(a, b) if(is.null(a)) b else rbind(a, b), init = NULL) # 结果合并为数据框,组合成当前中心点.cs,即准中心点
-    if (all(abs(cs - .cs) < eps)) # 当两次计算的聚簇中线点的坐标键的误差小于规定误差限度时终止算法，
+    # 依据两次中心点cs,.cs之间的相对关系决定下一步操作：终止结束还是继续进行
+    if (all(abs(cs - .cs) < eps)) # 当两次计算的聚簇中线点的坐标间的误差小于规定误差限度时终止算法，
          .cs # 中心点收敛，算法结束
     else loop ( if (nrow(.cs) < k) centeroids_gen(flag) # 中心点结构不完整，重新生成假定中心点已被再次重试 
                 else .cs ) # 不收敛则再次循环, 继续调整
