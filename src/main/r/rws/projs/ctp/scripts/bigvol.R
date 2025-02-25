ea38b69fd731806807af4b0f4eeced697932bb85
gbench <gbench@sina.com>
2025-02-26 04:27:32 +0800

    修改注释

--- src/main/r/rws/projs/ctp/scripts/bigvol.R
+++ src/main/r/rws/projs/ctp/scripts/bigvol.R
@@ -2,35 +2,65 @@
 # 提取 交易数据中的 大成交量 项目（包含时刻）
 # ----------------------------------------------------------------------
 
+library(tidyverse)
+library(data.table)
+library(xts)
+
 #' 一个将输入字符串内容转换成POSIXct日期格式的函数
-as.datetime <- partial(as.POSIXct, format="%H:%M:%S") # 时间分析
+as.datetime <- partial(as.POSIXct, format = "%H:%M:%S") # 时间分析
 # K线数据的提取
-kdata <- if (F) { # 使用文件数据还是数据库数据
-      # 当前工作区中的数据文件（以保证返回的文件路径都是相对于当前工作区的路径，而不是一个`不可直接访问`的简单字符串）
-      data.files <- list.files(path=".", all.files=T, recursive=T, include.dirs=F); data.files # 数据文件集合
-      # 读取指定数据(data.files的最后一项即最新的数据文件)
-      fread(last(data.files)) # 数据文件读取
-    } else { # 读取数据库数据
-      "t_rb2505_20250225" |> sprintf(fmt="select * from %s")|> sqlquery.h10ctp2()   
-  } |> compute_kline() |> (\(x, kd=x['T21:00/T23:00'], ix=index(kd))  # 提取指定时间段内的数据&并清除噪音数据
-      kd[ # 小时跨度的时间跨度过滤 
-          !( ix>as.datetime("15:00:00") & ix<as.datetime("21:00:00")) |
-          !( ix>as.datetime("11:30:00") & ix<as.datetime("13:30:00")) |
-          !( ix>as.datetime("10:15:00") & ix<as.datetime("10:30:00"))
-      ]) (); kdata# 精选时段数据 
+kdata <- (if (T) { # 使用文件数据还是数据库数据
+  # 当前工作区中的数据文件（以保证返回的文件路径都是相对于当前工作区的路径，而不是一个`不可直接访问`的简单字符串）
+  data.files <- list.files(path = ".", all.files = T, recursive = T, include.dirs = F)
+  data.files # 数据文件集合
+  # 读取指定数据(data.files的最后一项即最新的数据文件)
+  fread(last(data.files)) # 数据文件读取
+} else { # 读取数据库数据
+  "t_rb2505_20250225" |>
+    sprintf(fmt = "select * from %s") |>
+    sqlquery.h10ctp2()
+}) |>
+  compute_kline() |>
+  (\(x, kd = x["T09:00/T23:00"], ix = index(kd)) # 提取指定时间段内的数据&并清除噪音数据
+  kd[ # 小时跨度的时间跨度过滤
+    !(ix > as.datetime("15:00:00") & ix < as.datetime("21:00:00")) |
+      !(ix > as.datetime("11:30:00") & ix < as.datetime("13:30:00")) |
+      !(ix > as.datetime("10:15:00") & ix < as.datetime("10:30:00"))
+  ])()
+kdata # 精选时段数据
 
 # ----------------------------------------------------------------------------------------------------------------------------------
 
-# 提取交易数据（xts格式) 
+# 显示K线图
+ggplot(kdata, aes(seq(Close), Close)) +
+  geom_line() +
+  scale_x_continuous(
+    n.breaks = 10,
+    labels = partial(sapply, FUN = \(i) index(kdata)[i] |> strftime("%H:%M"))
+  )
+
+# 提取交易数据（xts格式)
 vol <- kdata$Volume # 获取成交量数据
 x <- as.numeric(vol) # 转换成向量
 
 # 计算经验累积分布函数
-empirical_cdf <-  x |> ecdf() # 结算经验分布函数
+empirical_cdf <- x |> ecdf() # 结算经验分布函数
 
 # 大单交易
 bigvols <- vol[empirical_cdf(as.numeric(x)) > 0.95] # 获得大单交易量
-print(bigvols) # 显示大胆交易交易数量
+# 显示大成交量
+dbl <- as.numeric # 转换成字符串数值向量的简写
+# 对于每一笔的大单成交量，一般来说都会对应着一次价格的大幅波动，因为，这往往意味着多空双方至少有一方
+# 在其报价盘口的头部档位中，其买卖数量的增幅，较其先前出了现大幅增加，当此种增幅 形成了某种单边趋势时，
+# 就必然会吞噬掉对方在其报价盘口中足够多的报价数量，若该吞噬能够彻底的将对方盘口挡位中头部价格的报价数量
+# 完全吃掉，成交价格的波动即涨跌幅就出现了，且势头越强，涨跌幅也就也越大。然而，这也不是尽然，不可一概而论，
+# 因为, 这里还会有双边趋势的时候，也就是双方同时都加大了投入力度，造成了旗鼓相当的局面，即当市场分歧较大时，
+# 多头与空头可能同时抛单，结果就造成了，尽管成交量放大了，价格却变化不大的局面。
+# 如果收盘价接近最低C_LO,说明 空头趋势 -1
+# 如果收盘价接近最高C_HI,说明 多头趋势 1
+kdata[index(bigvols)] |> # 
+  transform(C_LO = dbl(Close - Low), C_HI = dbl(Close - High)) |> # xts 需要用dbl转换成数字,否则C_LO，C_HI命名无效
+  transform(Desc = ifelse(abs(dbl(C_LO)) <= abs(dbl(C_HI)), -1, 1)) # 另外 这里必须使用transform mutate 不可以
 
 bigvols2 <- vol[x > quantile(x, 0.95)]
 # 百分位数与经验分布计算的数据时一样
