95fd4ae760b3266849399d1f6a5e9b39e9382c60
gbench <gbench@sina.com>
2025-03-15 16:31:38 +0800

    修改注释

--- src/main/r/rws/projs/inventory/mastdata.R
+++ src/main/r/rws/projs/inventory/mastdata.R
@@ -8,9 +8,9 @@
 # 产品列表
 products <- c("苹果" = "apple001", "香蕉" = "banana001", "草莓" = "strawberry001") # 产品列表
 # 公司列表
-companies <- c("中国第一出口公司" = "CMPN001", "华北进出口公司" = "CMPN002") # 公司
+companies <- c("沃尔玛" = "CMPN001", "亚马逊" = "CMPN002") # 公司
 # 仓库列表
-warehouses <- c("北京仓库" = "WRHS001", "上海仓库" = "WRHS002") # 产品列表
+warehouses <- c("北京京邦达贸易有限公司" = "WRHS001", "顺丰控股股份有限公司" = "WRHS002") # 产品列表
 
 # 默认统计公式
 default_path <- "cbind(total_in, total_out, qty, times) ~ name + date + company_id + warehouse_id"
