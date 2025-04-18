422890f347530db00c3fa38f5671ebf9c44b661d
gbench <gbench@sina.com>
2025-04-18 19:29:33 +0800

    修改注释

--- src/test/java/gbench/sandbox/ctp/NcCtp.java
+++ src/test/java/gbench/sandbox/ctp/NcCtp.java
@@ -37,17 +37,17 @@ public class NcCtp {
 	 */
 	public void connect(final String host, final int port) {
 		try (final Socket socket = new Socket(host, port);
-				final BufferedReader sysinReader = new BufferedReader(new InputStreamReader(System.in));
-				final OutputStream socketOs = socket.getOutputStream();
-				final InputStream socketIs = socket.getInputStream();
-				final BufferedReader socketBr = new BufferedReader(new InputStreamReader(socketIs))) {
+				final BufferedReader sysin_reader = new BufferedReader(new InputStreamReader(System.in));
+				final OutputStream socket_os = socket.getOutputStream();
+				final InputStream socket_is = socket.getInputStream();
+				final BufferedReader socket_br = new BufferedReader(new InputStreamReader(socket_is))) {
 
 			final AtomicBoolean flag = new AtomicBoolean(false);
 
 			final Thread readerThread = new Thread(() -> {
 				try {
 					while (!flag.get()) {
-						final var c = socketBr.read(); // 读取字符
+						final var c = socket_br.read(); // 读取字符
 						if (c < 0) {
 							// 连接已关闭，退出循环
 							break;
@@ -62,11 +62,12 @@ public class NcCtp {
 				}
 				println("READER EXIT!");
 			});
+			readerThread.setName("readerThread");
 
 			final Thread writerThread = new Thread(() -> {
 				try {
 					while (!flag.get()) {
-						String line = sysinReader.readLine();
+						String line = sysin_reader.readLine();
 						if (line.equals("quit")) {
 							flag.set(true);
 							// 关闭套接字以中断readerThread的阻塞
@@ -77,18 +78,21 @@ public class NcCtp {
 							}
 							break;
 						}
-						socketOs.write((line + "\n").getBytes());
-						socketOs.flush();
+						socket_os.write((line + "\n").getBytes());
+						socket_os.flush();
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				println("WRITER EXIT!");
 			});
+			readerThread.setName("writerThread");
 
+			// 读写线程
 			readerThread.start();
 			writerThread.start();
 
+			// 等待线程运行完毕
 			writerThread.join();
 			readerThread.join();
 
