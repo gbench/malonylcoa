a02e8abf7f0f087fd72ff1ce160ddf17da5711b1
gbench <gbench@sina.com>
2025-04-28 08:49:56 +0800

    增加文件

---
+++ src/main/r/rws/projs/ctp/quantmod/apl/mastdata.R
@@ -0,0 +1,20 @@
+# ------------------------------------------------------------------------------------
+# INVENTORY 库存&存货统计程序-主数据对象
+#
+# author:gbench@sina.com
+# date: 2025-03-12
+# ------------------------------------------------------------------------------------
+
+symbols <- c("螺纹钢2505"="rb2505", "甲醇505"="MA505")
+startime <- "09:00" 
+endtime <- "10:00"
+current_date <- "2025-02-28"
+dbhosts <- c("localhost"="localhost", "192.168.1.10"="192.168.1.10")
+dbnames <- c("ctp"="ctp", "ctp2"="ctp2", "ctp4"="ctp4")
+
+# 随兴&自定义环境
+env_adhoc <- new.env() # 函数环境
+update.adhoc <- \(dbhost, dbname) { # 环境更新
+  env_adhoc$sqlquery.adhoc <- \(...) sqlquery(host = dbhost, dbname = dbname, ...) # 数据库查询函数
+}
+update.adhoc(dbhosts[1], dnames[1]) # 随兴&自定义环境
