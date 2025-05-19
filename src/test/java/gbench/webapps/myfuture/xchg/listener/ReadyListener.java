92bcba9fdfe17e5de3351e83ff1aff20671d0c04
徐清华 <gbench@sina.com>
2025-05-20 01:19:54 +0800

    修改注释

--- src/test/java/gbench/webapps/myfuture/xchg/listener/ReadyListener.java
+++ src/test/java/gbench/webapps/myfuture/xchg/listener/ReadyListener.java
@@ -13,6 +13,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import gbench.util.data.MyDataApp;
+import gbench.util.io.Output;
 import gbench.util.lisp.DFrame;
 import gbench.util.lisp.IRecord;
 import gbench.webapps.myfuture.xchg.msclient.DataApiClient;
@@ -46,59 +47,57 @@ public class ReadyListener implements ApplicationListener<ApplicationReadyEvent>
 									.sorted(IRecord.cmp("PRICE,CREATE_TIME", true, true)); // 价格正，时间正序列
 							println("-- LONGS:%s".formatted(longs));
 							println("-- SHORTS:%s".formatted(shorts));
-							this.matchOrders(longs, shorts);
+							this.matchOrders(longs, shorts); // 撮合订单
 						});
-			}
+			} // while
 		});
 		thread.setDaemon(true);
 		thread.start();
 	}
 
 	/**
+	 * 撮合订单
 	 * 
-	 * @param longs
-	 * @param shorts
+	 * @param longs  多头头寸
+	 * @param shorts 空头头寸
 	 */
 	public void matchOrders(final DFrame longs, final DFrame shorts) {
-		final var rb = IRecord.rb(MATCHORDER_KEYS);
 		if (longs.nrows() < 1 || shorts.nrows() < 1) {
 			return;
 		}
+
+		final var rb = IRecord.rb(MATCHORDER_KEYS);
 		final var securityid = longs.head().get("SECURITY_ID");
 
-		for (final var lo : longs) {
-			final var loid = lo.str("ID");
+		for (final var lo : longs) { // 多头
+			final var lo_id = lo.str("ID");
 			final var lo_price = lo.i4("PRICE");
 			final var lo_quantiy = lo.i4("QUANTITY");
 
-			for (final var so : shorts) {
-				final var soid = so.str("ID");
+			for (final var so : shorts) { // 空头
+				final var so_id = so.str("ID");
 				final var so_price = so.i4("PRICE");
 				final var so_quantity = so.i4("QUANTITY");
-				final var now = LocalDateTime.now();
 
-				if (lo_price < so_price) { // 买价低于卖价
+				if (lo_price < so_price) { // 买价低于卖价：无法进行撮合
 					break;
 				} else { // 买价大于等于卖价
-					final var price = Math.min(lo_price, so_price);
-					final var quantity = Math.min(lo_quantiy, so_quantity);
-					final var datarec = rb.get(loid, soid, securityid, price, quantity, now, now, "撮合交易单");
-					final var insql = MyDataApp.insert_sql("t_match_order", datarec.toMap());
+					final var now = LocalDateTime.now();
+					final var price = Math.min(lo_price, so_price); // 撮合价格
+					final var quantity = Math.min(lo_quantiy, so_quantity); // 撮合数量
+					final var datarec = rb.get(lo_id, so_id, securityid, price, quantity, now, now, "撮合交易单"); // 撮合单数据
+					final var insql = MyDataApp.insert_sql("t_match_order", datarec.toMap()); // 数据插入语句
 
-					dataClient.sqlexecute(insql).subscribe(e -> {
-						println(insql);
-						println(e);
-					});
+					dataClient.sqlexecute(insql).subscribe(Output::println);
 				} // if
-			}
-		}
+			} // for so
+		} // for lo
 	}
 
 	@Autowired
 	DataApiClient dataClient;
 
 	final static String MATCHORDER_KEYS = "LONG_ORDER_ID,SHORT_ORDER_ID,SECURITY_ID,PRICE,QUANTITY,CREATE_TIME,UPDATE_TIME,DESCRIPTION";
-
 	final static String SECURITY_SQL = "select distinct SECURITY_ID from t_order";
 	final static String UNMATCHED_ORDER_SQL = """
 				select * from t_order where SECURITY_ID=$0 and ID not in (
