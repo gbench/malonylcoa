31a5b5b7864bbcad26d923782179fe3862954361
gbench <gbench@sina.com>
2025-11-24 14:37:37 +0800

    增加占比过滤

--- src/main/r/rws/projs/ctp/shinyapps/app04/asset_analyze.R
+++ src/main/r/rws/projs/ctp/shinyapps/app04/asset_analyze.R
@@ -35,15 +35,16 @@ batch_load()
 tickdata <- \(ratio=c(0.3, 0.5), instrument="t_rb2601_20251124", dbname="ctp", 
               host="127.0.0.1", begtime="09:00", endtime="12:00") {
   
+  .ratio <- sort(c(0,0.3,0.9,1)) |> (\(x) ifelse(x>=1 | x<=0, NA, x))() |> na.omit()    # 占比排序
   # 数据分组的标签生成
-  lbls <- ratio |> append(values=_, x=c(0, 1), after=1) |> (\(xt, x0=lag(xt)) cbind(x0, xt)[-1, ])()|> 
+  lbls <- .ratio |> append(values=_, x=c(0, 1), after=1) |> (\(xt, x0=lag(xt)) cbind(x0, xt)[-1, ])()|> 
     apply(1, paste, collapse=", ") |> sprintf(fmt="(%s]") # 绘制区间式样
   
   # 获取价格数据，Id:数据主键，LastPrice：最新成交价格，Volume：当日累计成交量，Vol:期间成交量，UpdateTime：hms解构数据更新时间
   "select Id, LastPrice, Volume, Volume-lag(Volume) over() Vol, UpdateTime from %s where UpdateTime between '%s' and '%s'" |>
     sprintf(fmt=_, instrument, begtime, endtime) |> # 凭借成有效的SQL
     sqlquery(dbname=dbname, host=host) |> with({ # 依据Id(数据主键)进行分组处理
-    Id |> (\(x) range(x) |> append(quantile(x, ratio), 1))() |> 
+    Id |> (\(x) range(x) |> append(quantile(x, .ratio), 1))() |> 
       cut(Id, breaks=_, include.lowest=T, labels=lbls) |> 
       split(price_data, f=_) # 区间分组
   });
