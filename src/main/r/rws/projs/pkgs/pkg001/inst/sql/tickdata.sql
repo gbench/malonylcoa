1b20f9ee7b8d31a77cabdb7b4853d75bfc33b3ee
gbench <gbench@sina.com>
2025-11-27 14:08:24 +0800

    增加多表操作

--- src/main/r/rws/projs/pkgs/pkg001/inst/sql/tickdata.sql
+++ src/main/r/rws/projs/pkgs/pkg001/inst/sql/tickdata.sql
@@ -1,15 +1,34 @@
 -- ---------------------------------------------------------------
--- 根据指定表名提取交易源数据
+-- 根据指定表名提取交易原数据
 -- # tickdata
 -- ##tbl 表名
 -- ---------------------------------------------------------------
 SELECT
     Id id, -- 主键索引
-    CAST(unix_timestamp(time(UpdateTime)) as UNSIGNED) x, -- 时间索引
+    -- 计算毫秒级时间戳：秒级时间戳*1000 + 毫秒数
+    UNIX_TIMESTAMP(STR_TO_DATE(CONCAT(TradingDay, " ", UpdateTime), '%Y%m%d %H:%i:%s')) * 1000 + UpdateMillisec AS x, -- 毫秒级时间戳
     LastPrice y, -- 价格
     COALESCE(Volume-LAG(VOLUME) OVER(), 0)  vol, -- 成交量
     STR_TO_DATE(REGEXP_SUBSTR('##tbl', '\\d{8}$'), '%Y%m%d')  date, -- 日期
     UpdateTime time, -- 更新日期
-    REGEXP_SUBSTR('##tbl', '(?<=t_).*?(?=_\\d{8}$)' ) symbol -- 合约代码
+    REGEXP_SUBSTR('##tbl', '(?<=t_).*?(?=_\\d{8}$)') symbol -- 合约代码
 FROM ##tbl
+
+-- ---------------------------------------------------------------
+-- 根据指定表名提取交易原数据
+-- # tickdata2
+-- ##tbl 表名
+-- ##startime  开始时间
+-- ##endtime 结束时间
+-- ---------------------------------------------------------------
+SELECT
+    Id id, -- 主键索引
+    -- 计算毫秒级时间戳：秒级时间戳*1000 + 毫秒数
+    UNIX_TIMESTAMP(STR_TO_DATE(CONCAT(TradingDay, " ", UpdateTime), '%Y%m%d %H:%i:%s')) * 1000 + UpdateMillisec AS x, -- 毫秒级时间戳
+    LastPrice y, -- 价格
+    COALESCE(Volume-LAG(VOLUME) OVER(), 0)  vol, -- 成交量
+    STR_TO_DATE(REGEXP_SUBSTR('##tbl', '\\d{8}$'), '%Y%m%d')  date, -- 日期
+    UpdateTime time, -- 更新日期
+    REGEXP_SUBSTR('##tbl', '(?<=t_).*?(?=_\\d{8}$)') symbol -- 合约代码
+FROM ##tbl WHERE UpdateTime BETWEEN #startime AND #endtime
 
