2de5a3e7dafac9142cfc1eb6e1df9df46c9407db
gbench <gbench@sina.com>
2025-12-18 12:16:51 +0800

    使用动态环境加载

--- src/main/r/rws/methods/attach.R
+++ src/main/r/rws/methods/attach.R
@@ -36,10 +36,14 @@ getOption("sqlquery.dbname") # 读取配置
 sqlquery("select database()") # 执行SQL语句
 
 # ------------------------------------------------------------------------------
-# xxxconfig的环境启动
+# xxxconfig的环境启动（使用dbplyr来使用数据库计算引擎来计算)
 # ------------------------------------------------------------------------------
+batch_load() # 导入基础库
+ss("magrittr,dbplyr") |> setNames(nm=_) |> lapply(require, character.only=T) |> unlist() # 加载dbplyr的基础库
+xxxconfig <- "pgconfig" # 为环境命名
+
 # attach环境xxxconfig，加载位置pos为".SqlQueryEnv"之的直接后继，进而可以拦截base::getOption以便读取自定义的系统配置！
-attach(new.env(), "xxxconfig", pos=match(".SqlQueryEnv", search()) + 1) |> with({ # 在环境xxxconfig里定制相关的系统配置
+attach(new.env(), xxxconfig, pos=match(".SqlQueryEnv", search()) + 1) |> with({ # 在环境xxxconfig里定制相关的系统配置
     localcfg <- list(sqlquery.drv=RPostgres::Postgres(),sqlquery.dbname="latinus", sqlquery.host="127.0.0.1", sqlquery.port=5432, 
         sqlquery.user="postgres", sqlquery.password="123456", sqlquery.schema="economics") # 本地环境参数设置
     getOption <- \ (x, default = NULL) localcfg[[x]] %||% base::getOption(x, default) # 联合base图层做叠化绘图的PS技法
@@ -52,18 +56,25 @@ sqlquery.pg("select current_schema") # 当前schema是economics
 sqlquery.ms <- partial(sqlquery, drv=MySQL(), host="127.0.0.1", port=3371, dbname="ctp", user="root", password="123456", 
     prettify = \(x) if (1 == length(x)) x[[1]] else bind_rows(x)) # 合并多张表为一张表
 
-# 使用框架处理程序来执行dbplyr数据操作！
-rb2605 <- (\(con, .log) { # 使用通用数据库连接管理框架dbfun.pg来托管数据库连接来进行dbply方式的合约处理
-      .rb2605 <- sqlquery.ms("show tables") |> grep(pattern="rb2605", value=T, x=_) |> # 读取数据
-          tail(2) |> sprintf(fmt="select * from %s limit 10") |> sqlquery.ms() # 从mysql里读取数据合约
-      copy_to(con, .rb2605) # 把从mysql获得数据数据上传到postgresql数据库
-      mtcars2 <- tbl(con, ".rb2605") |> collect() # 数据查询
-  }) |> dbfun.pg(f=_, search_path = getOption("sqlquery.schema"), verbose=F) # 合约处理
+# 使用框架处理程序dbfun.pg来生成一段sql执行计划来处理数据，
+# 注意：dbfun.pg的设计初衷是了执行SQL语句的函数来提供数据库连接的，因为他的生成函数必须要有一个sql参数！
+# 但是由于我们在处理过程中从未使用该sql参数，因此我们也就没有必要在应用rb2605.statistic统计时候写出！因为R的参数处理事Promise惰性求值的方式！
+rb2605.statistic <- (\(con, .log) { # 使用通用数据库连接管理框架dbfun.pg来托管数据库连接来进行dbply方式的合约处理
+    .rb2605 <- sqlquery.ms("show tables") |> grep(pattern="rb2605", value=T, x=_) |> # 读取数据
+        tail(2) |> sprintf(fmt="select * from %s limit 1000") |> sqlquery.ms() # 从mysql里读取数据合约
+    copy_to(con, .rb2605, name = "t_rb2605", overwrite = TRUE, temporary = FALSE)  
+    # 临时加：打印表结构，确认字段名/类型
+    print(dbGetQuery(con, 'SELECT "TradingDay", "UpdateTime" FROM t_rb2605 LIMIT 1') |> tibble())  
+    tbl(con, "t_rb2605") |> group_by(minute=concat(ActionDay, substr(replace(UpdateTime, ":", ""), 1, 4))) |> # 使用PG函数处理时间
+        summarise(mean=avg(LastPrice), n=n()) %T>% {print(sql_render(.))} |> arrange(desc(minute)) |> # 统计分钟数据
+        collect() # 生成SQL并执行结果
+}) |> dbfun.pg(search_path=getOption("sqlquery.schema"), verbose=F) # 合约处理
 
-# 数据执行数据
-rb2605() |> print()
+# 计算rb2605统计量
+rb2605.statistic() 
 
-detach("xxxconfig") # 卸载环境
+# 使用变量xxxconfig来动态卸载
+which(xxxconfig==search())|> as.list() |> do.call(detach, args=_)  # 卸载环境
 
 # ------------------------------------------------------------------------------
 # 工作区的配置
