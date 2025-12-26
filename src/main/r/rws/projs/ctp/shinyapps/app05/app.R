76a2c4d44abf02845b39f247aa171f606dc708e2
gbench <gbench@sina.com>
2025-12-26 19:42:46 +0800

    增加文件

--- src/main/r/rws/projs/ctp/shinyapps/app05/app.R
+++ src/main/r/rws/projs/ctp/shinyapps/app05/app.R
@@ -36,12 +36,12 @@ ui <- fluidPage(
     mainPanel(width = 10,
       tabsetPanel(id = "tabs", selected = "js",
         tabPanel("js",
-          tags$div(id = "chart", style = "width:100%;height:600px;border:1px solid #ccc;"),
+          tags$div(id = "chart", style = "width:100%;height:500px;border:1px solid #ccc;"),
           # 关键：把 JS 放在这个 tab 里，确保 DOM 已存在
           tags$script(src = "klinecharts.min.js"),
           tags$script(src = "kline.js")
         ),
-        tabPanel("R", plotOutput("kline", height = "600px"))
+        tabPanel("R", plotOutput("kline", height = "500px"))
       )
     )
   )
