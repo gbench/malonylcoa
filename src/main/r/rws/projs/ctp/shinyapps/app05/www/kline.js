d3e6502d4e5a4ed75ea17f0659076b98395c817f
gbench <gbench@sina.com>
2025-12-26 20:33:14 +0800

    修改注释

--- src/main/r/rws/projs/ctp/shinyapps/app05/www/kline.js
+++ src/main/r/rws/projs/ctp/shinyapps/app05/www/kline.js
@@ -1,39 +1,11 @@
 // www/kline.js
 (function(){
   const chart = klinecharts.init('chart');
-
   /* 1. 副图成交量 */
   chart.createIndicator('VOL');
-
-  /* 2. 注册 5 条 EMA 覆盖指标（主图） */
-  function addEMA(period, color){
-    chart.createIndicator(
-      {
-        name: 'MA',
-        calcParams: [period],
-        regenerateFigures: () => [
-          {
-            key: 'ma',
-            title: 'EMA' + period,
-            type: 'line',
-            baseValue: 0,
-            styles: { line: { color: color, size: 1 } }
-          }
-        ],
-        calc: (klineDataList) =>
-          klineDataList.map(k => ({ ma: k['ema' + period] }))
-      },
-      true,                       // overlay
-      { id: 'candle_pane' }       // 主图 pane
-    );
-  }
-
-  addEMA(1,  '#FF6B6B');
-  addEMA(5,  '#F9C74F');
-  addEMA(15, '#90BE6D');
-  addEMA(30, '#43AA8B');
-  addEMA(60, '#277DA1');
-
+  /* https://klinecharts.com/guide/indicator#%E5%86%85%E7%BD%AE%E6%8A%80%E6%9C%AF%E6%8C%87%E6%A0%87 */
+  chart.createIndicator('MA', true, { id:'candle_pane' }) 
+  chart.createIndicator('BOL', true, { id:'candle_pane' }) 
   /* 3. 监听 Shiny 推送 */
   Shiny.addCustomMessageHandler('push', j => chart.applyNewData(j));
 })();
