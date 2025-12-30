b1104d6140eda911983e0a918177824d3319bc3f
gbench <gbench@sina.com>
2025-12-30 14:53:32 +0800

    加入动态合约注入功能

--- src/main/r/rws/projs/ctp/shinyapps/app05/app.R
+++ src/main/r/rws/projs/ctp/shinyapps/app05/app.R
@@ -7,8 +7,7 @@ ui <- fluidPage(
   titlePanel("Ignite K-line"),
   sidebarLayout(
     sidebarPanel(width = 2,
-      selectInput("sym", "合约", choices=sqlquery("select table_name from system.tables") |> 
-        grep("KL", x=_, value=T) %>% setNames(nm=sub("", "", x=.)), selected = "KL_RB2605"),
+      selectInput("sym","合约",choices=NULL),
       numericInput("intv", "刷新(s):", 1, min = .1, step = .1),
       actionButton("refresh", "手动刷新"),
       hr(),
@@ -31,6 +30,9 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   tick <- reactiveTimer(500) # 定时计时周期，因为CTP推送聚合tickdata就是一秒两次的500毫秒的推送周期！
   update_klinechart <- \(data) session$sendCustomMessage("push", data) # 更新k线
+  safefech <- \(...) if(input$sym %in% syms()) fetch_json(...) else toJSON(list())
+  syms <- reactive({ input$refresh; sqlquery("select table_name nm from system.tables order by nm") |> 
+    grep("KL", x=_, value=T) %>% setNames(nm=sub("", "", x=.)) }) 
   data <- reactive({  # K线数据
     klines(input$sym) |> with(as.xts(cbind(OPEN, HIGH, LOW, CLOSE, VOLUME), 
       order.by=as.POSIXct(TS, format="%Y%m%d%H%M"))) |> tail(100)
@@ -39,10 +41,12 @@ server <- function(input, output, session) {
   # quantmod 近期绘图
   output$kline <- renderPlot(data() |> chartSeries(name=toupper(input$sym), theme=chartTheme("white")))
   
+  # 动态合约注入
+  observe(updateSelectInput(session, "sym", choices=syms(), selected="KL_RB2605")) |> bindEvent(syms()) # ③ 一行注入
   # 定时刷新klinechart（增量更新数据）
-  observe(update_klinechart(fetch_json(input$sym))) |> bindEvent(tick())
+  observe(update_klinechart(safefech(input$sym))) |> bindEvent(tick())
   # 切换合约（使用全量更新数据）
-  observe(update_klinechart(fetch_json(input$sym, startime=0))) |> bindEvent(input$sym)
+  observe(update_klinechart(safefech(input$sym, startime=0))) |> bindEvent(input$sym)
   # 手动刷新
   observe(invalidate_kline_caches()) |> bindEvent(input$refresh)
   # 退出事件
