1ea2061da894f309542aa610eac12cb6db5558d5
徐清华 <gbench@sina.com>
2026-01-16 10:44:46 +0800

    修改注释

--- src/test/java/gbench/sandbox/jdbc/JdbcIgniteTest.java
+++ src/test/java/gbench/sandbox/jdbc/JdbcIgniteTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
