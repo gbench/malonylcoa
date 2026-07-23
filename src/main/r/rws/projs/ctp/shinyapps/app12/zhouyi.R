de7eb6865fe7e34156bfc9c096494e20a02c0e3e
gbench <gbench@sina.com>
2026-07-23 10:02:09 +0800

    修改注释

--- src/main/r/rws/projs/ctp/shinyapps/app12/zhouyi.R
+++ src/main/r/rws/projs/ctp/shinyapps/app12/zhouyi.R
@@ -37,8 +37,8 @@ kqry <- \(pattern, ds=gs, prefix="【", suffix="】") ds |> grep(pattern, x=_, v
   gsub(gettextf("(.*)(%s)(.*)",pattern), gettextf("\\1%s\\2%s\\3", prefix, suffix), x=_) # 关键词查询
 trigrams <- list(坤=c(0,0,0), 震=c(0,0,1), 坎=c(0,1,0), 兑=c(0,1,1), 艮=c(1,0,0), 离=c(1,0,1), 巽=c(1,1,0), 乾=c(1,1,1)) # 三爻符号
 ynm <- list(X1="初爻", X2="二爻", X3="三爻", X4="四爻", X5="五爻", X6="上爻") # 爻辞名称
-taiji <- \(flag=T) list(ys=pick(xs,4) |> unlist() |> strsplit("上|下") |> lapply(\(i) trigrams[i])) |> # 提取八卦结构 
-  with(if(flag) {zs <- lapply(ys, unlist) |> data.frame(); attr(zs, "row.names") <- rev(names(ynm)); zs} else ys) # 八卦图
+taiji <- \(flag=T) list(ys=pick(xs,4) |> unlist() |> strsplit("上|下") |> lapply(\(i) trigrams[i])) |> # 提取太极结构 
+  with(if(flag) {zs <- lapply(ys, unlist) |> data.frame(); attr(zs, "row.names") <- rev(names(ynm)); zs} else ys) # 太极图
 yaos <- t(taiji()) # 爻辞
 
 # 初九 
