ddb3491bda48ba56d9b6a4afa8251e3f1fd8777d
gbench <gbench@sina.com>
2026-09-07 03:10:12 +0800

    优化代码

--- src/main/r/rws/scripts/finance/kline.R
+++ src/main/r/rws/scripts/finance/kline.R
@@ -293,10 +293,10 @@ calcKDJ <- function(x, n=9, m1=3, m2=3) {
 # addKDJ <- newTA(calcKDJ, col = c("purple", "orange", "green"), lwd = c(1.5, 1.5, 1.5), legend = "KDJ")
 
 # 基础K线图表
-kchart <- \(instrument="ma610", startime=0, endtime=24, periods=c(10, 20, 30), tsp=NULL) {
+kchart <- \(instrument=getOption("sqlquery.rb.instrument", "rb2701"), startime=0, endtime=24, periods=c(10, 20, 30), tsp=NULL) {
   rbx.tse(instrument, startime, endtime, tsp=tsp) |> evalq() |> sqldframe(x=OHLCV.1M) |> df2xts(3:8) |> chartSeries(name=instrument) # K线图表
-  newTA(SMA, Oi, on = NA, col = "grey50", type = "l", legend="OpenInterest") (n=1) |> print() # 带有持仓量的K线图，chob带有plot/print方法，需被R的自动打印触发
-  (\(n=periods, col=seq(n)+1) mapply(\(n, col, leg) newTA(SMA, Cl, on=1, legend=leg, col=col) (n=.(n)) |> bquote() |> eval(), n, col, leg=paste0("MA", n)) ) () # 多周期移动平均，legend 的名称参数被替换
+  newTA(SMA, Oi, col = "grey50", legend="OpenInterest") (n=1) |> print() # 带有持仓量的K线图，chob带有plot/print方法，需被R的自动打印触发
+  ( \(n=periods, col=seq(n)+1) mapply(\(n, col, lgd) newTA(SMA, Cl, on=1, legend=lgd, col=col) (n=.(n)) |> bquote() |> eval(), n, col, lgd=paste0("MA", n)) ) () # 多周期移动平均，legend 的名称参数被替换
 }
 
 # ctp tickdata sql
