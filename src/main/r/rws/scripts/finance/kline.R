708eb274474fffc0ba3b65611214d7e59cdadcc3
gbench <gbench@sina.com>
2026-09-07 14:41:04 +0800

    修改注释

--- src/main/r/rws/scripts/finance/kline.R
+++ src/main/r/rws/scripts/finance/kline.R
@@ -294,7 +294,8 @@ calcKDJ <- function(x, n=9, m1=3, m2=3) {
 
 # 基础K线图表
 kchart <- \(instrument=getOption("sqlquery.rb.instrument", "rb2701"), startime=0, endtime=24, periods=c(10, 20, 30), tsp=NULL) {
-  kct <- rbx.tse(instrument, startime, endtime, tsp=tsp) |> evalq() |> sqldframe(x=OHLCV.1M) |> df2xts(3:8) |> chartSeries(name=instrument) # K线图表
+  inst <- as.character(substitute(instrument)) # 提取原始合约符号，以便让 kchart(rb2701) 可以无障碍运行
+  kct <- rbx.tse(instrument, startime, endtime, tsp=tsp) |> substitute() |> eval() |> sqldframe(x=OHLCV.1M) |> df2xts(3:8) |> chartSeries(name=tryCatch(instrument, error=\(e) inst )) # K线图表
   ( range(kct@xdata$OpenInterest) |> newTA(SMA, Oi, col = "grey50", legend="OpenInterest", yrange=_) ) (n=1) |> print() # 带有持仓量的K线图，chob带有plot/print方法，需被R的自动打印触发
   ( \(n=periods, col=seq(n)+1) mapply(\(n, col, lgd) newTA(SMA, Cl, on=1, legend=lgd, col=col) (n=.(n)) |> bquote() |> eval(), n, col, lgd=paste0("MA", n)) ) () |> print() # 多周期移动平均，legend 的名称参数被替换
   kct |> invisible()
